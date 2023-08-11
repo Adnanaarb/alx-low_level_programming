4b88ccfdd48c0b48cb6ef4d79ed0bdd277df2baf
Your Name <you@example.com>
2023-08-11 13:57:24 -0700

    variables_if_else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,13 +14,13 @@ int main(void)
         n = rand() - RAND_MAX / 2;
         /* your code goes there */
         if (n>0){
-                return ("%d is positive",n);
+                return ("%d is positive\n",n);
         }         
         else if (n==0){
-                return ("%d is zero",n);
+                return ("%d is zero\n",n);
         }
-        else if (n<0){
-                return ("%d is negative",n);    
+        else {
+                return ("%d is negative\n",n);    
         }
         return (0);
 }
