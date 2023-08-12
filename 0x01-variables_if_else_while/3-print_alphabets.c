dcdd2744ebafaa0136f12d52bbef3aa41ade47f1
Your Name <you@example.com>
2023-08-12 06:37:39 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase and uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void){
+	char l,L;
+	for (l='a';l<='z';l++)
+        putchar(l);
+        	for (L='A';L<='Z';L++){
+        	putchar(L);
+        	}
+    
+
+    return 0;
+}
