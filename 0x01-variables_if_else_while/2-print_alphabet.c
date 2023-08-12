d49c82166961f75be5538821046a1c3f89bb54f1
Your Name <you@example.com>
2023-08-12 06:04:08 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,12 @@
 
 /**
  *main -> print the alphabet in lowercase
+ *
  *Return: Always 0 (Success)
  */
-int main(){
+int main(void){
 	char L;
-	for (L = 'a';L = 'z';L++)
+	for (L = 'a'; L <= 'z'; L++)
 		putchar(L);
 	putchar('\n');
 	return (0);
